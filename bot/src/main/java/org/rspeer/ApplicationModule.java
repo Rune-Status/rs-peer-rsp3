db8dd7a80eb3dd997464e71eaf9cff2c2281cf1c
Lezor <rawr-+4@live.com>
2020-07-28 01:32:09 +0100

    we dont need this? :D perhaps consider removing environment module or replacing it with something along the lines of "profile". currently it only consists of preferences but in addition to this we could put quickstart info, breaks and account info there

--- bot/src/main/java/org/rspeer/ApplicationModule.java
+++ bot/src/main/java/org/rspeer/ApplicationModule.java
@@ -2,7 +2,6 @@ package org.rspeer;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
-import org.rspeer.environment.BotContext;
 import org.rspeer.environment.preferences.BotPreferences;
 import org.rspeer.environment.preferences.JsonBotPreferencesLoader;
 import org.rspeer.event.EventDispatcher;
@@ -19,8 +18,6 @@ public class ApplicationModule extends AbstractModule {
                 .toInstance(EventDispatcher.Factory.getDefault("bot"));
 
         bind(ScriptController.class).asEagerSingleton();
-        bind(BotContext.class).asEagerSingleton();
-
         bind(BotPreferences.class).toInstance(new JsonBotPreferencesLoader().load());
         bind(BotFrame.class).asEagerSingleton();
         bind(BotMenuBar.class).asEagerSingleton();
